8be845a188801807ee5adbc157d3d6850ccd3801
israel Dembele <demisrael00@gmail.com>
2019-10-18 13:43:11 -0400

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,19 @@
-$(document).ready(function() { 
-var timeLeft= 20;
-var downloadTimer = setInterval(function() => {document.getElementById("countdown").innerHTML= time remaining + "second remaining";
- timeLeft -=1;   
- if (timeLeft <=0){
+$(document).ready(function(){ 
+var timeLeft = 20; 
+console.log( timeLeft);
+var downloadTimer = setInterval(function()={
+    document.getElementById("countdown").innerHTML = TimeRemaining + "second remaining";
+ TimeRemaining -= 1;   
+ if (TimeRemaining <=0){
      clearInterval(downloadTimer); 
      document.getElementById("countdown").innerHTML="Done"
  }
-}, 1000);
+});  
+
+var correctAnswers = 0; 
+var incorrectAnswers = 0;
+var unanswered = 0
+
 
 
 
